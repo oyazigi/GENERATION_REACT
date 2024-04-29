56ac23e756c8905c9af062d593c52566acf79bb3
oyazigi <mambo0001@outlook.com>
2024-04-29 15:09:06 -0300

    adding files for task 3

--- blog_pessoal/src/App.tsx
+++ blog_pessoal/src/App.tsx
@@ -1,3 +1,6 @@
+import ReactDOM from "react-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Card from '../components/Card/Card';
 import Task from "../components/Task/Task"
 import './App.css'
@@ -5,11 +8,12 @@ import './App.css'
 function App() {
 
   return (
-    <>
-     <Card titulo = "React" descricao = "biblioteca com JS"/>
-     <Card titulo = "Spring" descricao = "Framework de java"/>
-     <Task/>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path = "/cards" element = {<Card titulo = "Rotas" descricao = "Elemento carregado a partir da rota especificada"/>}/>
+     <Route path = "/task1" element = {<Task/>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
