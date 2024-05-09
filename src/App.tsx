9efcd2e351b4a5fa14701af8dc854fbdea51526e
oyazigi <mambo0001@outlook.com>
2024-05-09 09:27:45 -0300

    adding files from task8

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import Cadastro from './pages/cadastro/Cadastro';
 import Home from './pages/home/Home';
 import DeletarTema from './components/temas/deletarTema/DeletarTema';
 import Footer from './components/footer/Footer';
+import Perfil from './pages/perfil/Perfil';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
               <Route path="/cadastroTema" element={<FormularioTema />} />
               <Route path="/editarTema/:id" element={<FormularioTema />} />
               <Route path="/deletarTema/:id" element={<DeletarTema />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -25,8 +25,8 @@ function Navbar() {
             <div className='hover:underline'>Postagens</div>
             <Link to='/temas' className='hover:underline'>Temas</Link>
             <Link to='/cadastroTema' className='hover:underline'>Cadastrar tema</Link>
-              <div className='hover:underline'>Perfil</div>
-              <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
+            <Link to='/perfil' className='hover:underline'>Perfil</Link>
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
              
             </div>
           </div>
